1c9c7dbf6c3f65c41b0035fbe4a3d897e4d18469
hoodielive <hoodielive@gmail.com>
2017-04-25 01:39:53 -0400

    Initial Commit of JavaMe Application

---
+++ src/JavaMa.java
@@ -0,0 +1,12 @@
+// JavaMa Application  
+
+public class JavaMa 
+{
+	
+	// main methods begin execution of the java application 
+	public static void main(String[] args) 
+	{
+		System.out.println("This is a Java Application for Fun"); 
+	}
+
+}
