728b9ccf7bf658de2969f4f9cf5f20f5227696bf
hoodielive <hoodielive@gmail.com>
2020-01-26 22:19:10 -0500

    learning java

---
+++ fundamentals/relearn_java.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.security.*;
+
+public class FalsePiety {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println( "Integer: " + 10 + " Double: " + 3.14 + " Boolean: " + true);
+	  System.out.print("Hello ");
+	  System.out.print("World");
+	  System.out.printf("pi = %.5f", Math.PI);
+	}
+}
